a1322ee5a621d8ca765fde722b392acf6476d635
willemsteller <33094399+willemsteller@users.noreply.github.com>
2026-06-15 11:00:56 +0200

    Load username and avatar on home
    Signed-off-by: willemsteller <33094399+willemsteller@users.noreply.github.com>

--- Polytoria/scripts/mobile/ui/views/home/ViewHomePage.cs
+++ Polytoria/scripts/mobile/ui/views/home/ViewHomePage.cs
@@ -2,6 +2,8 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
+using Godot;
+using Polytoria.Datamodel;
 using Polytoria.Mobile.Utils;
 using Polytoria.Schemas.API;
 
@@ -9,13 +11,17 @@ namespace Polytoria.Mobile.UI;
 
 public partial class ViewHomePage : MobileViewBase
 {
-	//private Label _usernameLabel = null!;
-	//private PolytorianModel _polytorian = null!;
+	[Export] private Label _usernameLabel = null!;
+	private PolytorianModel _polytorian = null!;
 
 	public override void _EnterTree()
 	{
+		_polytorian = new();
+		GetNode("PlayerModel").AddChild(_polytorian.GDNode);
+		_polytorian.InitEntry();
+
 		PolyMobileAuthAPI.UserAuthenticated += OnUserAuthenticated;
-		//_polytorian.AvatarLoaded += OnAvatarLoaded;
+		_polytorian.AvatarLoaded += OnAvatarLoaded;
 
 		base._EnterTree();
 	}
@@ -23,16 +29,16 @@ public partial class ViewHomePage : MobileViewBase
 	public override void _ExitTree()
 	{
 		PolyMobileAuthAPI.UserAuthenticated -= OnUserAuthenticated;
-		//_polytorian.AvatarLoaded -= OnAvatarLoaded;
+		_polytorian.AvatarLoaded -= OnAvatarLoaded;
 
 		base._ExitTree();
 	}
 
-	private static void OnAvatarLoaded()
+	private void OnAvatarLoaded()
 	{
-		//((Node3D)_polytorian.GDNode).Visible = true;
-		//_polytorian.Animator.PlayOneShotAnimation("poly_welcome");
-		//_polytorian.SetState(CharacterModel.CharacterState.Idle);
+		((Node3D)_polytorian.GDNode).Visible = true;
+		_polytorian.Animator?.PlayOneShotAnimation("Wave");
+		_polytorian.SetState(CharacterModel.CharacterModelStateEnum.Idle);
 	}
 
 	private void OnUserAuthenticated(APIMeResponse response)
@@ -40,19 +46,19 @@ public partial class ViewHomePage : MobileViewBase
 		LoadView();
 	}
 
-	private static void LoadView()
+	private void LoadView()
 	{
-		//_usernameLabel.Text = PolyMobileAuthAPI.CurrentUserInfo.Username;
-		//_polytorian.LoadAppearance(PolyMobileAuthAPI.CurrentUserInfo.Id);
+		_usernameLabel.Text = PolyMobileAuthAPI.CurrentUserInfo.Username;
+		_polytorian.LoadAppearance(PolyMobileAuthAPI.CurrentUserInfo.Id);
 	}
 
 	public override void ShowView(object? args)
 	{
-		//((Node3D)_polytorian.GDNode).Visible = false;
-		//if (_polytorian.IsAvatarLoaded)
-		//{
-		//	OnAvatarLoaded();
-		//}
+		((Node3D)_polytorian.GDNode).Visible = false;
+		if (_polytorian.IsAvatarLoaded)
+		{
+			OnAvatarLoaded();
+		}
 		base.ShowView(args);
 	}
 }
